eba9039b0f2e6d0460ebfc36e1e9122d5e1856ed
ujjwal gupta <ugupta17k@gmail.com>
2026-02-21 17:11:38 +0000

    Q6 completed

---
+++ JS-basics/Q6.js
@@ -0,0 +1,26 @@
+//Group people by city
+//{ Delhi: ["A", "C"], Mumbai: ["B"] }
+
+const arr = [
+  { name: "A", city: "Delhi" },
+  { name: "B", city: "Mumbai" },
+  { name: "C", city: "Delhi" }
+]
+
+function GroupPeople(){
+    let grouped = {
+        Delhi :[],
+        Mumbai : [],
+    }
+    for(let i = 0; i<arr.length ; i++){
+        if(arr[i].city == "Delhi"){
+            grouped.Delhi.push(arr[i].name)
+        }
+        if(arr[i].city == "Mumbai"){
+            grouped.Mumbai.push(arr[i].name)
+        }
+    }
+    return grouped;
+}
+let ans = GroupPeople(arr)
+console.log(ans);
